f9ed10311de9416e3ddacce3f8fce83e1c06ca72
yashusomani25-prog <yashusomani25@gmail.com>
2026-05-28 03:56:48 +0545

    Update app.py

--- app.py
+++ app.py
@@ -948,4 +948,5 @@ def create_tables():
 create_tables()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
